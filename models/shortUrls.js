36cc46a8a55027f046e58f2895ec99777093e392
Prajna1999 <gituprajna20@gmail.com>
2022-09-07 08:53:08 +0530

    add routes and db

---
+++ models/shortUrls.js
@@ -0,0 +1,25 @@
+//db schema.
+import mongoose from "mongoose";
+import shortid from "shortid";
+
+
+export const shortUrlSchema=new mongoose.Schema({
+    full:{
+        type:String,
+        required:true
+
+    },
+    short:{
+        type:String,
+        required:true,
+        default:shortid.generate()
+    },
+    clicks:{
+        type:Number,
+        required:true,
+        default:0
+    }
+
+})
+
+
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import express from "express";
+import mongoose from "mongoose";
+// import { shortUrlSchema } from "./models/shortUrls";
+export {shortUrlSchema} from './models/shortUrls.js'
+
 
 const app=express();
 
+mongoose.connect('mongodb://localhost/urlShortener', {
+    useNewUrlParser:true, useUnifiedTopology:true
+})
 //set ejs view engine
 
 app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({extended:false}))
 //route for index
-app.get('/', (req,res)=>{
-    res.render('index')
+app.get('/',async (req,res)=>{
+    const ShortUrls=await shortUrlSchema.find();
+
+    res.render('index', {shortUrls:ShortUrls})
+})
+
+//route for shortUrls. DB Connect.
+app.get('/shortUrls' , async (req, res)=>{
+    await shortUrlSchema.create({full:req.body.fullUrl});
+
+    res.redirect('/')
 })
 
 app.listen(process.env.PORT||5000);
